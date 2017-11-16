0aafe0754b6ca11d90dc21ea82fcb29b05cb460c
Zama Mohammed <zmohammed@rocketfuel.com>
2017-11-16 13:47:46 -0600

    observables added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/observable/merge';
+
+
+import { map, distinctUntilChanged, filter } from 'rxjs/operators';
+import { startWith } from 'rxjs/operators/startWith';
+import { scan } from 'rxjs/operators/scan';
+import { mapTo } from 'rxjs/operators/mapTo';
 
 @Component({
   selector: 'gs-root',
@@ -8,4 +18,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'gs';
   version = '2.2.1';
+  time;
+
+  click$ = new Subject();
+
+  constructor() {
+    Observable.merge(
+      Observable.interval(1000).pipe(mapTo('seconds')),
+      this.click$.pipe(mapTo('minutes'))
+    )
+      .pipe(
+        startWith(<any>new Date()),
+        scan((acc: Date, curr: string) => {
+          const result: Date = new Date(acc.getTime());
+          if (curr === 'seconds') {
+            result.setSeconds(acc.getSeconds() + 1);
+          }
+
+          if (curr === 'minutes') {
+            result.setMinutes(acc.getMinutes() + 1);
+          }
+          return result;
+        })
+      )
+      .subscribe(val => this.time = val);
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,6 +3,14 @@ import { GithubService } from '../github.service';
 import { environment } from '../../environments/environment';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/map';
+import { Subject } from 'rxjs/Subject';
+import { filter, flatMap,  } from 'rxjs/operators';
+import { debounceTime } from 'rxjs/operators/debounceTime';
+import { map } from 'rxjs/operators/map';
+import { switchMap, } from 'rxjs/operators/switchMap';
+import { distinctUntilChanged } from 'rxjs/operators/distinctUntilChanged';
+import { startWith } from 'rxjs/operators/startWith';
+
 
 @Component({
   selector: 'gs-search',
@@ -11,21 +19,34 @@ import 'rxjs/add/operator/map';
 })
 export class SearchComponent implements OnInit {
   user: any = {};
-
+  text$ = new Subject();
   constructor(private githubService: GithubService) { }
 
   ngOnInit() {
-    this.searchUser(environment.user);
-  }
-  searchUser(username) {
-    this.githubService.getUser(username)
-    .switchMap((user: any) =>
-      this.githubService.getRepos(user.login).map(repos => ({
-        ...user,
-        repos
-      }))
+    // this.searchUser(environment.user);
+    this.text$
+    .pipe(
+      startWith(environment.user),
+      debounceTime(500),
+      filter((val: string) => val.length > 4),
+      distinctUntilChanged(),
+      switchMap((val) => this.githubService.getUser(val).switchMap((user: any) =>
+          this.githubService.getRepos(user.login).map(repos => ({
+            ...user,
+            repos
+          }))))
     )
-    .subscribe(user => this.user = user);
+      .subscribe(user => this.user = user);
   }
+  // searchUser(username) {
+  //   this.githubService.getUser(username)
+  //   .switchMap((user: any) =>
+  //     this.githubService.getRepos(user.login).map(repos => ({
+  //       ...user,
+  //       repos
+  //     }))
+  //   )
+  //   .subscribe(user => this.user = user);
+  // }
 
 }
